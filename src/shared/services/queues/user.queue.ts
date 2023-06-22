230757ec119566e2883e30b3192ae9a605ebc474
patryk.krawiec <patryk.krawiec@itx.group>
2023-06-22 19:30:03 +0200

    feat: Sign up feat

--- src/shared/services/queues/user.queue.ts
+++ src/shared/services/queues/user.queue.ts
@@ -1,9 +1,10 @@
+import { userWorker } from '@worker/user.worker';
 import { BaseQueue } from './base.queue';
 
 class UserQueue extends BaseQueue {
   constructor() {
     super('user');
-    // this.processJob('addUserToDb', 5, userWorker.addUserToDB);
+    this.processJob('addUserToDB', 5, userWorker.addUserToDB);
   }
 
   public addUserJob(name: string, data: any): void {
